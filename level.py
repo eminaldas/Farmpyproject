57e4dbbc8bf5030004e1e9ee0f5a74f353c69043
Muhammed Emin Aldaş <eminaldas575@gmail.com>
2024-04-15 23:48:57 +0300

    carpismalar

--- level.py
+++ level.py
@@ -1,26 +1,54 @@
-import  pygame
+import pygame
 from settings import *
 from player import Player
 from overlay import Overlay
-from sprites import Generic
+from sprites import Generic,Water,WildFlower,Tree
 from pytmx.util_pygame import load_pygame
+from support import *
 
 class level:
     def __init__(self):
                                                                                 #get the display surface
         self.display_surface = pygame.display.get_surface()
 
-                                                                                #sprite group
-        self.all_sprites = CameraGroup()
-                                                                                #tüm canlıların çilmiesinde kolaylık sağlar
+
+        self.all_sprites = CameraGroup()                                          #sprite group
+        self.collision_sprites = pygame.sprite.Group()
+
         self.setup()
         self.overlay = Overlay(self.player)
-    def setup(self):
-       # tmx_data = load_pygame('./data/map.tmx')
 
-      #  tmx_data.get_layer_by_name('HouseFurnitureBottom').tiles()
-        self.player = Player((640,360),self.all_sprites)                   #oyuncunun konumunu ekranda belirtip çiziyor
-        Generic(pos =(0,0),
+    def setup(self):
+        tmx_data = load_pygame('./data/map.tmx')
+        # ev
+        for layer in ['HouseFloor', 'HouseFurnitureBottom']:
+            for x, y, surf in tmx_data.get_layer_by_name(layer).tiles():
+                Generic((x*TILE_SIZE, y*TILE_SIZE), surf, self.all_sprites, LAYERS['house bottom'])
+
+        for layer in ['HouseWalls', 'HouseFurnitureTop']:
+            for x, y, surf in tmx_data.get_layer_by_name(layer).tiles():
+                Generic((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites)
+
+        # çit
+        for x, y, surf in tmx_data.get_layer_by_name('Fence').tiles():
+            Generic((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites)
+
+        # su
+
+        water_frames = import_folder('./graphics/water')
+        for x, y, surf in tmx_data.get_layer_by_name('Water').tiles():
+            Water((x * TILE_SIZE, y * TILE_SIZE), water_frames, [self.all_sprites,self.collision_sprites])
+
+        # çiçekler
+        for obj in tmx_data.get_layer_by_name('Decoration'):
+           WildFlower((obj.x, obj.y), obj.image, [self.all_sprites,self.collision_sprites])
+
+        # ağaçlar
+        for obj in tmx_data.get_layer_by_name('Trees'):
+            Tree((obj.x, obj.y), obj.image, [self.all_sprites,self.collision_sprites], obj.name)
+
+        self.player = Player((640, 360), self.all_sprites,self.collision_sprites)                   #oyuncunun konumunu ekranda belirtip çiziyor
+        Generic(pos =(0, 0),
                 surf = pygame.image.load('./graphics/world/ground.png'),
                 groups = self.all_sprites,
                 z = LAYERS['ground']
@@ -45,7 +73,7 @@ class CameraGroup(pygame.sprite.Group):
         self.offset.y = player.rect.centery - SCREEN_HEIGHT / 2
 
         for layer in LAYERS.values():
-            for sprite in self.sprites():
+            for sprite in sorted(self.sprites(),key = lambda sprite: sprite.rect.centery):
                 if sprite.z == layer:
                     offset_rect = sprite.rect.copy()
                     offset_rect.center -= self.offset
--- player.py
+++ player.py
@@ -4,7 +4,7 @@ from support import  *
 from timer import Timer
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self,pos,group):
+    def __init__(self,pos,group,collision_sprites):
         super().__init__(group)
 
         self.import_assets()#karakterin bütün pozisyonları için hazırlanan görseller buradan alınıyor
@@ -21,6 +21,9 @@ class Player(pygame.sprite.Sprite):
         self.pos = pygame.math.Vector2(self.rect.center)
         self.speed = 200
 
+        #çarpışmalar
+        self.hitbox = self.rect.copy().inflate((-126, -70))
+        self.collision_sprites = collision_sprites
         #Timers
         self.timers = {
             'tool use' : Timer(350,self.use_tool),
@@ -118,6 +121,26 @@ class Player(pygame.sprite.Sprite):
                 self.selected_seed = self.seeds[self.seeds_index]
                 print(self.selected_seed)
 
+    def collision(self, direction):
+        for sprite in self.collision_sprites.sprites():
+            if hasattr(sprite, 'hitbox'):
+                if sprite.hitbox.colliderect(self.hitbox):
+                    if direction == 'horizontal':
+                        if self.direction.x > 0:  # moving right
+                            self.hitbox.right = sprite.hitbox.left
+                        if self.direction.x < 0:  # moving left
+                            self.hitbox.left = sprite.hitbox.right
+                        self.rect.centerx = self.hitbox.centerx
+                        self.pos.x = self.hitbox.centerx
+
+                    if direction == 'vertical':
+                        if self.direction.y > 0:  # moving down
+                            self.hitbox.bottom = sprite.hitbox.top
+                        if self.direction.y < 0:  # moving up
+                            self.hitbox.top = sprite.hitbox.bottom
+                        self.rect.centery = self.hitbox.centery
+                        self.pos.y = self.hitbox.centery
+
     def move(self,dt):
 
         if self.direction.magnitude() > 0:                              #vektör çapraz harektlerde normalinden daha hızlı->
@@ -125,12 +148,15 @@ class Player(pygame.sprite.Sprite):
 
         #yatay hareket
         self.pos.x += self.direction.x * self.speed * dt
-        self.rect.centerx = self.pos.x
+        self.hitbox.centerx = round(self.pos.x)
+        self.rect.centerx = self.hitbox.centerx
+        self.collision('horizontal')
 
         #dikey hareket
         self.pos.y += self.direction.y * self.speed * dt
-        self.rect.centery = self.pos.y
-
+        self.hitbox.centery  = round(self.pos.y)
+        self.rect.centery = self.hitbox.centery
+        self.collision('vertical')
     def update(self,dt):
         #fonksiyonlar çağırılıyor
         self.input()
